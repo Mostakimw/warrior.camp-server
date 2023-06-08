b5b878277370a1d658073718837df5ccf1d5de1a
Mostakimw <scientist.expert@gmail.com>
2023-06-08 17:05:56 +0600

    user role updated api added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eh4qdyd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,7 +32,7 @@ async function run() {
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
-      const existingUser = await usersCollection.find(query);
+      const existingUser = await usersCollection.findOne(query);
       if (existingUser) {
         return res.send({ message: "user already exist" });
       }
@@ -42,6 +42,26 @@ async function run() {
     // ! getting all users
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // ! change user to admin or instructor
+    app.put("/users/:id/role", async (req, res) => {
+      const id = req.params.id;
+      const { role } = req.body;
+      console.log(role);
+      const query = { _id: new ObjectId(id) };
+      const currentUser = await usersCollection.findOne(query);
+      console.log(currentUser);
+      if (!currentUser) {
+        return res.status(404).send({ message: "user not found" });
+      }
+      const updateDoc = {
+        $set: {
+          role: role,
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc);
       res.send(result);
     });
 

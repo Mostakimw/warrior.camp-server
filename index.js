631034627d03057dc7d1b8463b8ab9a32e76ef21
Mostakimw <scientist.expert@gmail.com>
2023-06-09 14:06:24 +0600

    class details update api added

--- index.js
+++ index.js
@@ -87,6 +87,53 @@ async function run() {
       res.send(result);
     });
 
+    // ! get single class by their id
+    app.get("/classes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollection.findOne(query);
+      res.send(result);
+    });
+
+    // ! update class by instructor
+    app.patch("/classes/:id", async (req, res) => {
+      const id = req.params.id;
+      const classData = req.body;
+      console.log(classData);
+      const query = { _id: new ObjectId(id) };
+      //   const classDataSavedInDb = await classesCollection.findOne(query);
+      const updateDoc = {
+        $set: {
+          className: classData?.className,
+          classThumbnail: classData?.classThumbnail,
+          price: classData?.price,
+          availableSeats: classData?.availableSeats,
+          description: classData?.description,
+        },
+      };
+      const result = await classesCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // ! update class status
+    app.put("/classes/:id/status", async (req, res) => {
+      const id = req.params.id;
+      const { status } = req.body;
+      console.log(status);
+      const query = { _id: new ObjectId(id) };
+      const currentStatus = await classesCollection.findOne(query);
+      if (!currentStatus) {
+        return res.status(404).send({ message: "user not found" });
+      }
+      const updateDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await classesCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
